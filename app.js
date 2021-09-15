8762f6f7ef0997e2a638b54812c1f7b1e1254864
Rohit Prasad <pdrohit674@gmail.com>
2021-09-15 21:13:24 +0530

    Added error handler

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var output = document.querySelector("#output-txt");
 
 
 var serverUrl = "https://api.funtranslations.com/translate/pig-latin.json";
-// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function errorHandler(error){
+    console.log("error is " + error);
+    alert("something went wrong with the server");
+}
 
 function generateURL(text) {
     return serverUrl + "?" + "text=" + text;
@@ -13,5 +17,5 @@ btn.addEventListener("click", function() {
     var inputtext = txt.value;
     fetch(generateURL(inputtext)).then(response => response.json()).then(data => {
         output.innerText = data.contents.translated;
-    });
+    }).catch(errorHandler);
 });
